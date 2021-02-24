6a0f75e5cb19d6e3915c3b3ea9128d9b736c6089
havinhlong1988 <havinhlong1988@gmail.com>
2021-02-24 12:05:30 +0800

    Create main.m

---
+++ main.m
@@ -0,0 +1,7 @@
+close all; clear all; clc;
+
+%% Example of rotation the seismogram in sac format
+% 2 option of rotational
+%   + ZNE to ZRT
+%   + ZNE to LQT: using ZRT to rotate to LQT
+% ===========================================================================================
